86ebe867b413aef13240bcc6dc559aa9d43c7876
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2021-03-10 09:55:35 +0100

    ajout getScore 1

---
+++ CPP_KARAOKE_CAILLAUD/Player.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+#include "Player.h"
+
+void Player::getName()
+{
+    std::cin >> name;
+    std::cout << "Votre nom est" << name << std::endl;
+    return 0;
+}
+
+void Player::getScore1(int Score1)
+{
+
+}
+
+void Player::getScore2(int Score2)
+{
+    
+}
+
+void Player::getScore3(int Score3)
+{
+    
+}
+
+void Player::getScore4(int Score4)
+{
+    
+}
+
+void Player::getScore5(int Score5)
+{
+    
+}
+
+void Player::getBestScore(int bestScore)
+{
+    std::cout << "Votre meilleur score est de" << bestScore << std::endl;
+}
---
+++ CPP_KARAOKE_CAILLAUD/Player.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class Player{
+    private:
+    int Player::getName();
+
+    public:
+    int Player::getScore1();
+    int Player::getScore2();
+    int Player::getScore3();
+    int Player::getScore4();
+    int Player::getScore5();
+    int Player::getWorstScore();
+    int Player::getBestScore();
+};
