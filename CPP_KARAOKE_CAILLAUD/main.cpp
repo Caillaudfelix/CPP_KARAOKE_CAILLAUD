332386aa1c49d1c26de249dcb805f243b54febe8
Caillaudfelix <74654899+Caillaudfelix@users.noreply.github.com>
2021-03-10 10:42:03 +0100

    main 1

--- CPP_KARAOKE_CAILLAUD/main.cpp
+++ CPP_KARAOKE_CAILLAUD/main.cpp
@@ -6,4 +6,11 @@ using namespace std;
 int main(){
     Player ("Kid")
     Player.getName;
+    Player.getScore1;
+    Player.getScore2;
+    Player.getScore3;
+    Player.getScore4;
+    Player.getScore5;
+    Player.getBestScore;
+    Player.getWorstScore;
 }
